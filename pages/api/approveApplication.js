bb1ec6daac8842b2293c785c4ede3559e14f1166
Lacuna Strategies <lacunastrategies@gmail.com>
2023-06-05 16:40:27 -0700

    Update approveApplication.js
    Replaced node-canvas with @napi-rs/canvas

--- pages/api/approveApplication.js
+++ pages/api/approveApplication.js
@@ -26,7 +26,7 @@ const uploadClient = new Twitter({
 })
 
 // ** Canvas Imports
-import { createCanvas, loadImage, registerFont } from 'canvas'
+import { createCanvas, loadImage } from '@napi-rs/canvas'
 
 export default async function handler(req, res) {
 
