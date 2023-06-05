eb092859d145d8b1b84251ff7ac1635365d46a93
Lacuna Strategies <lacunastrategies@gmail.com>
2023-06-05 16:38:53 -0700

    Update approveNomination.js
    Remove canvas from nomination approval for clean testing on application approval

--- pages/api/approveNomination.js
+++ pages/api/approveNomination.js
@@ -25,8 +25,8 @@ const uploadClient = new Twitter({
     subdomain: 'upload'
 })
 
-// ** Canvas Imports
-const { createCanvas, loadImage, registerFont } = require('canvas')
+// // ** Canvas Imports
+// const { createCanvas, loadImage, registerFont } = require('canvas')
 
 export default async function handler(req, res) {
     const { method } = req
@@ -85,135 +85,135 @@ export default async function handler(req, res) {
 
 
             /* ==== Create Twitter Media ==== */
-            // Register Font
-            const fontFile = path.join(process.cwd(), 'fonts', 'Roboto-Bold.ttf')
-            registerFont(fontFile, { family: 'Roboto' })
+            // // Register Font
+            // const fontFile = path.join(process.cwd(), 'fonts', 'Roboto-Bold.ttf')
+            // registerFont(fontFile, { family: 'Roboto' })
 
-            // Canvas Size
-            const width = 1200
-            const height = 675
+            // // Canvas Size
+            // const width = 1200
+            // const height = 675
 
-            // Initiate canvas
-            const canvas = createCanvas(width, height)
-            const context = canvas.getContext('2d')
+            // // Initiate canvas
+            // const canvas = createCanvas(width, height)
+            // const context = canvas.getContext('2d')
 
-            // Load background image
-            try {
-                // Load image from assets and draw to canvas context as layer
-                const imageToLoad = nominee.role === 'Member' ? 'public/assets/images/member-bg-1200x675.jpg' : 'public/assets/images/senior-bg-1200x675.jpg'
-                const bgImage = await loadImage(imageToLoad)
-                context.drawImage(bgImage, 0, 0, width, height)
-            } catch (error) {
-                // If we encounter an error, log the error and apply a flat color background as fallback
-                console.log('Error Loading Bg Image => ', error)
-                context.fillStyle = '#000'
-                context.fillRect(0, 0, width, height)
-            }
+            // // Load background image
+            // try {
+            //     // Load image from assets and draw to canvas context as layer
+            //     const imageToLoad = nominee.role === 'Member' ? 'public/assets/images/member-bg-1200x675.jpg' : 'public/assets/images/senior-bg-1200x675.jpg'
+            //     const bgImage = await loadImage(imageToLoad)
+            //     context.drawImage(bgImage, 0, 0, width, height)
+            // } catch (error) {
+            //     // If we encounter an error, log the error and apply a flat color background as fallback
+            //     console.log('Error Loading Bg Image => ', error)
+            //     context.fillStyle = '#000'
+            //     context.fillRect(0, 0, width, height)
+            // }
 
-            // Load nominator profile image
-            try {
-                // Load image from assets and draw to canvas context as layer
-                context.save()
-                const profileImg = await loadImage(nominator.profilePic.replace('normal', '400x400'))
-                context.beginPath()
-                context.lineWidth = 20
-                context.strokeStyle = '#ffca13'
-                context.arc(338, 319, 143, 0, Math.PI * 2, false)
-                context.stroke()
-                context.clip()
-                context.drawImage(profileImg, 195, 176, 286, 286)
-                context.restore()
-            } catch (error) {
-                // If we encounter an error, log the error and apply a flat color background as fallback
-                console.log('Error Loading Nominator Profile Image => ', error)
-            }
+            // // Load nominator profile image
+            // try {
+            //     // Load image from assets and draw to canvas context as layer
+            //     context.save()
+            //     const profileImg = await loadImage(nominator.profilePic.replace('normal', '400x400'))
+            //     context.beginPath()
+            //     context.lineWidth = 20
+            //     context.strokeStyle = '#ffca13'
+            //     context.arc(338, 319, 143, 0, Math.PI * 2, false)
+            //     context.stroke()
+            //     context.clip()
+            //     context.drawImage(profileImg, 195, 176, 286, 286)
+            //     context.restore()
+            // } catch (error) {
+            //     // If we encounter an error, log the error and apply a flat color background as fallback
+            //     console.log('Error Loading Nominator Profile Image => ', error)
+            // }
 
-            // Load nominee profile image
-            try {
-                // Load image from assets and draw to canvas context as layer
-                context.save()
-                const profileImg = await loadImage(nomineeProfilePic.replace('normal', '400x400'))
-                context.beginPath()
-                context.lineWidth = 20
-                context.strokeStyle = '#ffca13'
-                context.arc(862, 319, 143, 0, Math.PI * 2, false)
-                context.stroke()
-                context.clip()
-                context.drawImage(profileImg, 719, 176, 286, 286)
-                context.restore()
-            } catch (error) {
-                // If we encounter an error, log the error and apply a flat color background as fallback
-                console.log('Error Loading Nominee Profile Image => ', error)
-            }
+            // // Load nominee profile image
+            // try {
+            //     // Load image from assets and draw to canvas context as layer
+            //     context.save()
+            //     const profileImg = await loadImage(nomineeProfilePic.replace('normal', '400x400'))
+            //     context.beginPath()
+            //     context.lineWidth = 20
+            //     context.strokeStyle = '#ffca13'
+            //     context.arc(862, 319, 143, 0, Math.PI * 2, false)
+            //     context.stroke()
+            //     context.clip()
+            //     context.drawImage(profileImg, 719, 176, 286, 286)
+            //     context.restore()
+            // } catch (error) {
+            //     // If we encounter an error, log the error and apply a flat color background as fallback
+            //     console.log('Error Loading Nominee Profile Image => ', error)
+            // }
 
-            // Set image text placeholders values
-            let text = ''
+            // // Set image text placeholders values
+            // let text = ''
 
-            // Set text to Nominator Twitter handle
-            text = `@${nominator.twitterHandle}`
+            // // Set text to Nominator Twitter handle
+            // text = `@${nominator.twitterHandle}`
 
-            // Set base text/font styles
-            context.textAlign = 'center'
-            context.font = '42px "Roboto"'
+            // // Set base text/font styles
+            // context.textAlign = 'center'
+            // context.font = '42px "Roboto"'
 
-            // Fill bottom drop shadow
-            context.fillStyle = 'rgba(0,0,0,0.3)'
-            context.fillText(text, 342, 529)
+            // // Fill bottom drop shadow
+            // context.fillStyle = 'rgba(0,0,0,0.3)'
+            // context.fillText(text, 342, 529)
 
-            // Fill top drop shadow
-            context.fillStyle = 'rgba(0,0,0,0.7)'
-            context.fillText(text, 340, 526)
+            // // Fill top drop shadow
+            // context.fillStyle = 'rgba(0,0,0,0.7)'
+            // context.fillText(text, 340, 526)
 
-            // Fill text
-            context.fillStyle = '#fff'
-            context.fillText(text, 338, 524)
+            // // Fill text
+            // context.fillStyle = '#fff'
+            // context.fillText(text, 338, 524)
 
-            // Set text to Nominee Twitter handle
-            text = `@${nominee.twitterHandle}`
+            // // Set text to Nominee Twitter handle
+            // text = `@${nominee.twitterHandle}`
 
-            // Set base text/font styles
-            context.textAlign = 'center'
+            // // Set base text/font styles
+            // context.textAlign = 'center'
 
-            // Fill bottom drop shadow
-            context.fillStyle = 'rgba(0,0,0,0.3)'
-            context.fillText(text, 866, 529)
+            // // Fill bottom drop shadow
+            // context.fillStyle = 'rgba(0,0,0,0.3)'
+            // context.fillText(text, 866, 529)
 
-            // Fill top drop shadow
-            context.fillStyle = 'rgba(0,0,0,0.7)'
-            context.fillText(text, 864, 526)
+            // // Fill top drop shadow
+            // context.fillStyle = 'rgba(0,0,0,0.7)'
+            // context.fillText(text, 864, 526)
 
-            // Fill text
-            context.fillStyle = '#fff'
-            context.fillText(text, 862, 524)
+            // // Fill text
+            // context.fillStyle = '#fff'
+            // context.fillText(text, 862, 524)
 
-            const dataUrl = canvas.toDataURL()
-            const base64 = dataUrl.split(',')[1]
+            // const dataUrl = canvas.toDataURL()
+            // const base64 = dataUrl.split(',')[1]
 
-            // const buffer = canvas.toBuffer('image/png')
-            // writeFileSync('generatedImages/test.png', buffer)
+            // // const buffer = canvas.toBuffer('image/png')
+            // // writeFileSync('generatedImages/test.png', buffer)
 
-            let mediaIdString = ''
-            try {
-                const { media_id_string } = await uploadClient.post('media/upload', {
-                    media_data: base64,
-                    media_category: 'TWEET_IMAGE',
-                })
-                mediaIdString = media_id_string
-            } catch (error) {
-                console.log('Media Upload to Twitter Failed => ', error)
-            }
+            // let mediaIdString = ''
+            // try {
+            //     const { media_id_string } = await uploadClient.post('media/upload', {
+            //         media_data: base64,
+            //         media_category: 'TWEET_IMAGE',
+            //     })
+            //     mediaIdString = media_id_string
+            // } catch (error) {
+            //     console.log('Media Upload to Twitter Failed => ', error)
+            // }
 
-            const parameters = {
-                media_ids: mediaIdString,
-                status: `Congratulations, @${nominee.twitterHandle}! You have been nominated by ${nominee.role === 'Member' ? 'Senior Scout' : 'Elite Scout'} @${nominee.nominatedBy} to be a ${nominee.role} on their team! Head on over to our website to accept your nomination${nominee.role === "Senior Scout" ? ' and start building your team' : ''}!`
-            }
+            // const parameters = {
+            //     media_ids: mediaIdString,
+            //     status: `Congratulations, @${nominee.twitterHandle}! You have been nominated by ${nominee.role === 'Member' ? 'Senior Scout' : 'Elite Scout'} @${nominee.nominatedBy} to be a ${nominee.role} on their team! Head on over to our website to accept your nomination${nominee.role === "Senior Scout" ? ' and start building your team' : ''}!`
+            // }
 
-            try {
-                const tweet = await twitterClient.post('statuses/update', parameters)
-            } catch (error) {
-                console.log('Error Posting Tweet! => ', error)
-                return res.status(500).json({ success: false, error })
-            }
+            // try {
+            //     const tweet = await twitterClient.post('statuses/update', parameters)
+            // } catch (error) {
+            //     console.log('Error Posting Tweet! => ', error)
+            //     return res.status(500).json({ success: false, error })
+            // }
 
             res.status(200).json({ success: true })
             break
